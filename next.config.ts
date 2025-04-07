2bcfe8545791c76024615be26c2cf02934de9ea2
Jacob Smith <jacob.d.smith@live.com>
2025-04-07 00:28:49 -0400

    Fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const config: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default config;
