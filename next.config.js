4ddfb9b9511602474be83ff2f9f8c2bc9e6174f4
Jacob Smith <jacob.d.smith@live.com>
2025-04-10 21:19:33 -0400

    fix: update next.config for static export

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'standalone', // Default, explicitly stating it might help.
-  // No output: 'export' here
-};
-
-export default nextConfig; 
